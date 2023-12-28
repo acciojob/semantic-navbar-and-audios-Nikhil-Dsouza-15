bd220772e5c7221202d23d2639a27197da0d4496
Nikhil-Dsouza-15 <nikhil.kr.dsouza9090@gmail.com>
2023-12-28 18:53:23 +0530

    done code changes for the given task

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+  // Sending HTML content dynamically
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3">Link 1</a></li>
+                <li><a href="http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg">Link 2</a></li>
+                <li><a href=" http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3">Link 3</a></li>
+            </ul>
+        </nav>
+    
+        <h2>3 random audios</h2>
+    
+        <audio controls>
+            <source src="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+    
+        <audio controls>
+            <source src="http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg" type="audio/ogg">
+            Your browser does not support the audio element.
+        </audio>
+    
+        <audio controls>
+            <source src="http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+    
+        <script type="text/javascript" src="./script.js"></script>
+    </body>
+    </html>
+  `;
+
+  res.send(htmlContent);
 });
-//your code here
+
 app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
+  const { a, b } = req.body;
+  res.status(200).send(`${a + b}`);
 });
+
 module.exports = app;
